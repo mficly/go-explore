013391879d25fa4db22a84e04cac44d3e48ab318
dai.song <mficly@msn.com>
2022-12-05 08:41:37 +0800

    1.change error processing format

--- db/connect.go
+++ db/connect.go
@@ -1,41 +1,46 @@
 package db
 
-import(
+import (
+	"context"
 	"log"
 	"os"
-	"context"
+
 	"github.com/joho/godotenv"
+
 	// "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func InitConnection(){
+func InitConnection() (string, error) {
 	runtime := os.Getenv("RUNTIME_ENV")
 	if runtime == "" {
-		panic("Can not get RUNTIME_ENV, aborting...")
+		return "Can not get RUNTIME_ENV, aborting...", nil
 	}
 	log.Printf("RUNTIME_ENV %v", runtime)
-	if err := godotenv.Load(".env."+ runtime); err != nil {
-		log.Println("No .env file found")
+	if err := godotenv.Load(".env." + runtime); err != nil {
+		// log.Println("No .env file found")
+		return "No .env file found", err
 	}
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environmental variable")
+		// log.Fatal("You must set your 'MONGODB_URI' environmental variable")
+		return "You must set your 'MONGODB_URI' environmental variable", nil
 	}
 	log.Printf("Connection String: %v", uri)
 
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
-		panic(err)
+		// panic(err)
+		return "can not connect to mongodb", err
 	}
 
 	defer func() {
 		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
+			// panic(err)
+			log.Fatal("can not disconnect to mongodb")
 		}
 	}()
 
-	
+	return "", nil
 }
-
